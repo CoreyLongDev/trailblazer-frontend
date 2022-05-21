60aae19c9901b82905eeb0235c3688900ab1d196
mylest913 <mylest913@gmail.com>
2022-05-21 12:15:45 -0700

    foote

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import { Index } from './Components/Content/Index';
+import { SearchBar } from './Components/Searchbar/SearchBar';
 import Footer from './Components/Footer/Footer';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-
-
 function App() {
   return (
     < >
     <Header />
-
-
-
-
-    <footer><Footer /></footer>
+    <SearchBar/>
+    <Index/>
+    <Footer/>
     </ >
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
