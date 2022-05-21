cb1b090c9943cdaf46dff2b24b086e95c90df932
Jacob Vernau <jacobvernau@gmail.com>
2022-05-21 11:57:58 -0600

    columns generated and responsive

--- src/Components/Content/Card.jsx
+++ src/Components/Content/Card.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
-export const Card = () => {
+export default function Card({ park }) {
   return (
-    <div>Card</div>
-  )
+    <MDBCard style={{ height: "100%", margin: ".5rem", minWidth: "20vw" }}>
+      <div
+        className="bg-image"
+        style={{ height: "15rem", textAlign: "center" }}
+      >
+        <MDBCardImage
+          style={{ height: "15rem" }}
+          src={park.images[0].url}
+          fluid
+          alt={park.name}
+        />
+      </div>
+      {/* <a>
+        <div
+          className="mask overlay"
+          style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+        ></div>
+      </a> */}
+      <MDBCardBody>
+        <MDBCardTitle>{park.name}</MDBCardTitle>
+        <MDBCardText>{park.description}</MDBCardText>
+        <div>
+          <MDBBtn href="#">View</MDBBtn>
+        </div>
+      </MDBCardBody>
+    </MDBCard>
+  );
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <App />
   
