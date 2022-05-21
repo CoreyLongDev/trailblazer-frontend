7575d2a2b3661295157a9dc703035a29420c0626
Jacob Vernau <jacobvernau@gmail.com>
2022-05-21 11:57:58 -0600

    columns generated and responsive

--- src/Components/Content/Index.jsx
+++ src/Components/Content/Index.jsx
@@ -1,39 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import { Card } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
 
 export const Index = () => {
-  
+  const [parks, setParks] = useState([]);
 
-  const [parks, setParks] = useState([])
+  console.log(parks);
 
-  console.log(parks)
+  const key = "EL1Bl0NrfZIjxhIIUVPEsrnWNz5o0cWaaKhvyWa7";
 
-  const key = "EL1Bl0NrfZIjxhIIUVPEsrnWNz5o0cWaaKhvyWa7"
-
-  const url = `https://developer.nps.gov/api/v1/parks?limit=10&api_key=${key}`
+  const url = `https://developer.nps.gov/api/v1/parks?limit=10&api_key=${key}`;
 
   useEffect(() => {
     fetch(url)
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-      setParks(res.data)
-    })
-    .catch(console.error)
-  },[])
-
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setParks(res.data);
+      })
+      .catch(console.error);
+  }, []);
 
   return (
     <>
-    {parks.map(park => 
-    <Card park = {park}/>
-    )}
-    {/* {parks.map((park) =>
+      <MDBContainer>
+        <MDBRow className="mb-3">
+          {parks.map((park) => (
+            <MDBCol
+              style={{ padding: "0", marginBottom: "1rem" }}
+              md="4"
+              className="col-example"
+            >
+              <Card park={park} />
+            </MDBCol>
+          ))}
+          {/* {parks.map((park) =>
   <div key={park.id}>
     <h1>{park.fullName}</h1>
     <h2>{park.latLong}</h2>
   </div>  
   )} */}
+        </MDBRow>
+      </MDBContainer>
     </>
-  )
-}
+  );
+};
