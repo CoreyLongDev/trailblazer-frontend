c98fea1fabd7358f1499a566a99bf3073386f864
PatriotxZero <Coreylong90@icloud.com>
2022-05-21 12:56:26 -0400

    changes to header folder and creation of header css. working on google fonts to get working

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
-    < >
-    <h1 id='header'>Trail Blazers</h1>
-    <h3 id='sub'>Find your next Destination. . .</h3>
-    </ >
-  )
-}
+    <>
+      <h1 id="header">Trail Blazers</h1>
+      <h3 id="sub">Find your next Destination. . .</h3>
+    </>
+  );
+};
 
-export default Header
+export default Header;
