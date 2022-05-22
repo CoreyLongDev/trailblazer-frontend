2877f3675b8853fd1749944a761368b50773302e
Myles <96808978+MJT30@users.noreply.github.com>
2022-05-21 23:17:02 -0700

    Add files via upload

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import '../Footer/footer.css'
+import { MDBIcon } from 'mdb-react-ui-kit';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      Trail Blazers&trade; <MDBIcon fas icon="map-marker-alt" /> <MDBIcon fas icon="map-signs" /> <MDBIcon fas icon="globe-americas" />
+    <p><a href='https://git.generalassemb.ly/flex-125/project-3/issues/18'>About </a> | <a href='https://www.nps.gov/subjects/developer/index.htm'>Api </a>| Contact us: <a href='https://www.linkedin.com/in/coreylong90/'>Cory Long </a> | <a href='https://www.linkedin.com/in/david-dobrose/'>David Dobrose  </a> | <a href='https://www.linkedin.com/in/jacobvernau/'>Jacob Vernau </a> | <a href='https://www.linkedin.com/in/martin-wilson-tel/'>Martin Wilson </a> | <a href='https://www.linkedin.com/in/myles-turner-b7629297/'>Myles Turner</a></p> 
+    
+    
+    
+
+    </div>
+
+    
   )
 }
+
+export default Footer
