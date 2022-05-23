0be6332075a78d25a473a5d88db7a67bc7796172
ddobr <ddobrose@yahoo.com>
2022-05-23 18:27:02 -0500

    fixed styling on navbar

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MDBNavbar,
+   MDBContainer,
+  MDBBreadcrumb,
+MDBBreadcrumbItem } from "mdb-react-ui-kit";
 
 const NavBar = () => {
+  
   return (
-    <div>
-      <ul className="navBar">
-        <li><Link className= "link" to={"/"}><h1>Home</h1></Link></li>
-        <li><Link className= "link" to={"/parks"}><h1>Find a Park</h1></Link></li>
-        <li><Link className= "link" to={"/about"}><h1>About Us</h1></Link></li>
-      </ul>
-    </div>
+    
+    <MDBNavbar expand='lg' light bgColor='light'>
+    <MDBContainer fluid>
+      <nav aria-label='breadcrumb'>
+        <MDBBreadcrumb>
+          <MDBBreadcrumbItem>
+          <Link className= "link" to={"/"}><a href="/">Home</a></Link>
+          </MDBBreadcrumbItem>
+          <MDBBreadcrumbItem>
+          <Link className= "link" to={"/parks"}><a href="/parks">Find a Park</a></Link>
+          </MDBBreadcrumbItem>
+          <MDBBreadcrumbItem active aria-current='page'>
+          <Link className= "link" to={"/about"}><a href="/about">About Us</a></Link>
+          </MDBBreadcrumbItem>
+        </MDBBreadcrumb>
+      </nav>
+    </MDBContainer>
+  </MDBNavbar>
   );
 };
 
