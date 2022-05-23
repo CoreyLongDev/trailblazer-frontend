9e0661b56bc0f3f1cca8418e6e9782e7ff09d92f
ddobr <ddobrose@yahoo.com>
2022-05-23 13:18:31 -0500

    added links to navbar

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
       <ul className="navBar">
-        <li>Home</li>
-        <li>???</li>
-        <li>About Us</li>
+        <li><Link className= "link" to={"/"}><h1>Home</h1></Link></li>
+        <li><Link className= "link" to={"/parks"}><h1>Find a Park</h1></Link></li>
+        <li><Link className= "link" to={"/about"}><h1>About Us</h1></Link></li>
       </ul>
     </div>
   );
