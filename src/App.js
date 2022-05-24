ad31f9c08c2c4f4247592fbfc27a9a400bab5184
Myles <96808978+MJT30@users.noreply.github.com>
2022-05-24 16:10:49 -0700

    Updated Footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { SearchBar } from "./Components/Searchbar/SearchBar";
 import { Routes, Route } from "react-router-dom";
 import Welcome from "./Pages/Welcome";
 import { About } from "./Pages/About";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <NavBar />
       <SearchBar />
       </header>
-      
+      <Footer />
       
 
       <Routes>
