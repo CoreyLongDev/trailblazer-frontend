a02b8b058c10284c1b951a12737f74dce96ad1d3
ddobr <ddobrose@yahoo.com>
2022-05-25 18:16:52 -0500

    fixed map

--- src/Components/map/Map.jsx
+++
@@ -1,9 +0,0 @@
-// import React, {useMemo} from 'react'
-// import {GoogleMap, useLoadScript,Marker } from "react-google-maps/api"
-
-// export const map = () => {
-//     const {} = useLoadScript
-//   return (
-//     <div>map</div>
-//   )
-// }
