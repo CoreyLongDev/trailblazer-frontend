14e164cca4f68bbbd5fe660dd3eebf5c6b8b1d27
mylest913 <mylest913@gmail.com>
2022-05-24 18:11:08 -0700

    p

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className='footer'>
       Trail Blazers&trade; <MDBIcon fas icon="map-marker-alt" /> <MDBIcon fas icon="map-signs" /> <MDBIcon fas icon="globe-americas" />
-    <p><a href='https://git.generalassemb.ly/flex-125/project-3/issues/18'>About </a> | <a href='https://www.nps.gov/subjects/developer/index.htm'>Api </a>| Contact us: <a href='https://www.linkedin.com/in/coreylong90/'>Cory Long </a> | <a href='https://www.linkedin.com/in/david-dobrose/'>David Dobrose  </a> | <a href='https://www.linkedin.com/in/jacobvernau/'>Jacob Vernau </a> | <a href='https://www.linkedin.com/in/martin-wilson-tel/'>Martin Wilson </a> | <a href='https://www.linkedin.com/in/myles-turner-b7629297/'>Myles Turner</a></p> 
+    <p><a href='https://git.generalassemb.ly/flex-125/project-3/issues/18'><i class="fab fa-github"></i> </a> | <a href='https://www.nps.gov/subjects/developer/index.htm'>Api </a>| Contact us: <a href='https://www.linkedin.com/in/coreylong90/'>Cory Long </a> | <a href='https://www.linkedin.com/in/david-dobrose/'>David Dobrose  </a> | <a href='https://www.linkedin.com/in/jacobvernau/'>Jacob Vernau </a> | <a href='https://www.linkedin.com/in/martin-wilson-tel/'>Martin Wilson </a> | <a href='https://www.linkedin.com/in/myles-turner-b7629297/'>Myles Turner</a></p> 
     
     
     
