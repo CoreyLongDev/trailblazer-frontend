2b8b7f38dc4b62db298477873b842543d438babe
Jacob Vernau <jacobvernau@gmail.com>
2022-05-25 21:01:17 -0600

    npi cards are styled horizontally

--- src/Components/Content/Card.jsx
+++ src/Components/Content/Card.jsx
@@ -11,41 +11,50 @@ import View from "../View/View";
 
 export default function Card({ park }) {
   return (
-    <MDBCard style={{ height: "100%", margin: ".5rem", minWidth: "20vw" }}>
-      <div style={{ height: "10rem", textAlign: "center" }}>
+    <MDBCard
+      style={{
+        height: "100%",
+        margin: ".5rem",
+        minWidth: "35vw",
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          minWidth: "125px",
+          maxWidth: "125px",
+          textAlign: "center",
+        }}
+      >
         <MDBCardImage
-          style={{ height: "10rem" }}
+          style={{ height: "100%", width: "100%", objectFit: "cover" }}
           src={park.images[0].url}
           fluid
           alt={park.name}
         />
       </div>
-      
-      <MDBCardBody>
-        <MDBCardTitle>{park.fullName}</MDBCardTitle>
+
+      <MDBCardBody style={{ textAlign: "right", paddingRight: "6px" }}>
+        <MDBCardTitle className="mb-0">{park.fullName}</MDBCardTitle>
         <MDBCardText>
           <div>
             {park.addresses[0].city}, {park.addresses[0].stateCode}
           </div>
         </MDBCardText>
         <div>
-            <Link to= {`/${park.id}/view` }state={park}>
-                
-        <button
-            style={{ position: "absolute", right: "0", bottom: "0" }}
-            href="#"
-            className="btn btn-primary"
-
-          >
-          
-            View
-          </button>
+          <Link to="/:id/view" state={{ from: "/" }}>
+            <button
+              style={{ position: "absolute", right: "0", bottom: "0" }}
+              href="#"
+              className="btn btn-md btn-primary"
+            >
+              Visit
+            </button>
           </Link>
         </div>
-        
       </MDBCardBody>
-      
     </MDBCard>
-    
   );
 }
