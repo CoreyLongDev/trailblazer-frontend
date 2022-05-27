69c3c24795345ff5c61363ffb3c6a8af917630ad
mylest913 <mylest913@gmail.com>
2022-05-26 20:10:12 -0700

    Api is working

--- src/Components/View/View.jsx
+++ src/Components/View/View.jsx
@@ -3,7 +3,7 @@ import {  useParams } from 'react-router-dom';
 import './view.css'
 // import { MDBIcon } from 'mdb-react-ui-kit';
 function View() {
-    const [park, setPark] = useState([]);
+    const [park, setPark] = useState(null);
     let {id} = useParams()
     const key = "EL1Bl0NrfZIjxhIIUVPEsrnWNz5o0cWaaKhvyWa7"
     const url = `https://developer.nps.gov/api/v1/parks?q=${id}&limit=10&api_key=${key}`;
@@ -11,7 +11,7 @@ function View() {
         fetch(url)
           .then((res) => res.json())
           .then((res) => {
-            console.log(res);
+            console.log(res.data[0]);
             setPark(res.data[0]);
           })
           .catch(console.error);
@@ -19,13 +19,16 @@ function View() {
   return (
 <>
     
-    <div className='fullName'>
+    {park && (
+      <>
+      <div className='fullName'>
           <h1>{park.fullName}</h1>
-          {/* <h4>{park.states}</h4> */}
+          <h4>{park.states}</h4>
+          <img height="100%" width="100%" src={park.images[0].url} alt ='image'></img>
       </div>
       
-{/* 
-      <span><h1>{park.fullName} ({park.states}) Facts</h1></span>
+
+      <span><h2>{park.fullName} ({park.states}) Facts</h2></span>
     <div className='parkfacts'>
     
     <strong>States: </strong> {park.states}           
@@ -43,11 +46,21 @@ function View() {
     <br></br>
    {park.activities[0].name}, {park.activities[1].name}, {park.activities[2].name}, {park.activities[3].name}, {park.activities[4].name}, {park.activities[5].name}
    
+   </div>
+
+   <div className='text'>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+<br></br>
+<br></br>
+  <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+
+   </div>
+      </>
+
+    )} 
+
    
-   
-   
-   
-    </div> */}
     
       
 </>
