ad48ae8d6d0c67764e9a008d90ddf161d1880e9e
Jacob Vernau <jacobvernau@gmail.com>
2022-05-31 17:51:32 -0600

    fixed css issues on welcome page & added margin to App.css for entire website

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -17,7 +17,11 @@ const Welcome = () => {
         style={{
           backgroundImage:
             "url('https://mdbootstrap.com/img/new/slides/041.webp')",
+          backgroundColor: "#cccccc" /* Used if the image is unavailable */,
           height: 400,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
           marginTop: ".5rem",
         }}
       >
@@ -27,12 +31,13 @@ const Welcome = () => {
             backgroundColor: "rgba(0, 0, 0, 0.6)",
             margin: "0 auto",
             top: "50%",
+            width: "75%",
             transform: "translateY(50%)",
           }}
         >
           <div className="d-flex justify-content-center align-items-center h-100">
             <div className="text-white">
-              <h1 className="mb-3">Trail Blazers</h1>
+              <h1 className="mb-3 text-white">Trail Blazers</h1>
               <hr
                 style={{
                   borderTop: "3px solid #bbb",
@@ -58,11 +63,10 @@ const Welcome = () => {
             <MDBCardImage
               overlay
               src="https://cdn.pixabay.com/photo/2016/11/22/19/25/man-1850181_960_720.jpg"
-              className="h-100"
               alt="trails"
             />
             <MDBCardOverlay>
-              <MDBCardTitle>Hike</MDBCardTitle>
+              <MDBCardTitle style={{ fontSize: "1rem" }}>Hike</MDBCardTitle>
             </MDBCardOverlay>
           </Link>
         </MDBCard>
@@ -76,10 +80,10 @@ const Welcome = () => {
               overlay
               src="https://cdn.pixabay.com/photo/2016/08/12/20/14/river-1589616_960_720.jpg"
               alt="national parks"
-              className="h-100"
+              style={{ height: "118%" }}
             />
             <MDBCardOverlay>
-              <MDBCardTitle>See</MDBCardTitle>
+              <MDBCardTitle style={{ fontSize: "1rem" }}>See</MDBCardTitle>
             </MDBCardOverlay>
           </Link>
         </MDBCard>
@@ -96,7 +100,7 @@ const Welcome = () => {
               className="h-100"
             />
             <MDBCardOverlay>
-              <MDBCardTitle>Soak</MDBCardTitle>
+              <MDBCardTitle style={{ fontSize: "1rem" }}>Soak</MDBCardTitle>
             </MDBCardOverlay>
           </Link>
         </MDBCard>
