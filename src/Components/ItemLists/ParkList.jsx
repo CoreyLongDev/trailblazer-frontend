4e7b3d49a577332ff526c40f422be4b9e1706ba5
tellydelly <martinwilson450@gmail.com>
2022-06-03 19:30:37 -0700

    please work

--- src/Components/ItemLists/ParkList.jsx
+++ src/Components/ItemLists/ParkList.jsx
@@ -2,25 +2,7 @@ import React, { useState, useEffect } from "react";
 import Card from "../Content/Card";
 import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
 
-const ParkList = ({setMapCenter}) => {
-  const [parks, setParks] = useState([]);
-
-  // const key = "EL1Bl0NrfZIjxhIIUVPEsrnWNz5o0cWaaKhvyWa7";
-
-  
-  // const url = `https://developer.nps.gov/api/v1/parks?&limit=10&api_key=${key}`;
-
-  const url = 'https://fathomless-eyrie-16229.herokuapp.com/parks';
-
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        // console.log(res);
-        setParks(res);
-      })
-      .catch(console.error);
-  }, []);
+const ParkList = ({setMapCenter, parks}) => {
 
   return (
     <>
